4dfcc77eb8e1d7ac03204e2fc8a30ff461573e54
Bowen Brinegar <bowenbrinegar@gmail.com>
2019-04-23 19:35:05 -0400

    added leaderboard

--- src/components/main/game-portal/leaderboard/index.js
+++ src/components/main/game-portal/leaderboard/index.js
@@ -6,12 +6,61 @@ import NotFound from './not-found'
 
 
 class StatsModal extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            data: null,
+            leaderboard: []
+        }
+
+        this.renderLeaderboard = this.renderLeaderboard.bind(this);
+    }
+
+    renderLeaderboard() {
+        const htmlArr = [];
+        const data = this.state.data
+
+        let i;
+        for (i = 0; i < 10; i++) {
+            const html = (
+                <div className='leader-pod'>
+                    <h3>{data[i].username} ---------------------------------------- {data[i].score}</h3>
+                </div>
+            );
+
+            htmlArr.push(html);
+        }
+
+        this.setState({
+            leaderboard: [...this.state.leaderboard, ...htmlArr]
+        });
+    }
+
+    componentDidMount() {
+        FlightBox.getFlightRecords().then(payload => {
+            payload = JSON.parse(payload);
+            
+            const data = payload.sort((a, b) => {
+                return b.score - a.score
+            });
+
+            this.setState({
+                data: data
+            }, this.renderLeaderboard)
+        })
+    }
 
     render() {
         return (
             <div className='leaderboard-wrapper'>
                 <div className="LeaderboardModalContainer">
-                    <NotFound />
+                    {this.state.leaderboard !== [] ? (
+                        <div className='wrapper'>
+                            {this.state.leaderboard}
+                        </div>
+                    ) : (
+                        <NotFound />
+                    )}
                 </div>
             </div>
         );
